54860ada37549a23ee602fc39a777b0743065a26
unknown <arguetaej@yahoo.com>
2020-03-11 20:24:48 -0400

    added componentdidmount method and shouldcomponent update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class Ticker extends Component
+{
+
+//constructor
+constructor ()
+{
+    super();
+    this.state ={
+      count: 0,
+                }
+
+}
+//methods
+increment = () =>{
+   if(this.state.count){
+     this.setState({count: this.state.count + this.state.i,
+                  })
+   }
 }
 
-export default App;
+clear = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+
+
+  componentDidMount(){
+setInterval(() =>{
+    this.setState({
+      count: this.state.count+1
+    })
+  },1000)
+}
+shouldComponentUpdate(nextProps,nextState){
+  if(nextState.count % 3 === 0) return true;
+  else return false;
+}
+//render
+render(){
+
+          return(
+
+                    <div>
+                    <p>this ticker is: <span>{this.state.count}</span></p>
+                    <button onClick={()=>this.setState({count:0})} type="button">Reset</button>
+                    </div>
+
+                );
+        };
+};
+
+
+export default Ticker;
---
+++ src/tick.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+setInterval (( ) =>{
+  console.log('Hello');
+}1000);
